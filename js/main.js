929d2f59832d1dd702d9205f490902547bdf1b0e
Navod Nirmal Nawarathna <novanawarathna@gmail.com>
2022-07-08 13:02:55 +0530

    update

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@
 })();
 
 function bodyScrollingToggle() {
-    document.body.classList.toggle("stop-scrolling");
+    document.body.classList.toggle("hidden-scrolling");
 }
 
 
@@ -69,19 +69,112 @@ function bodyScrollingToggle() {
             screenshots = portfolioItems[itemIndex].querySelector(".portfolio-item-img img").getAttribute("data-screenshots");
             //convert screenshots into array
             screenshots = screenshots.split(",");
+            if(screenshots.length === 1){
+                prevBtn.style.display="none";
+                nextBtn.style.display="none";
+            }
+            else{
+                prevBtn.style.display="block";
+                nextBtn.style.display="block";
+            }
             // console.log(screenshots)
             slideIndex = 0;
             popupToggle();
+            popupSlideshow();
+            popupDetails();
         }
     })
 
     closeBtn.addEventListener("click", ()=>{
         popupToggle();
+        if(projectDetailsContainer.classList.contains("active")){
+            popupDetailsToggle();
+        }
     })
 
     function popupToggle(){
         popup.classList.toggle("open");
         bodyScrollingToggle();
+    }
+
+    function popupSlideshow(){
+        const imgSrc = screenshots[slideIndex];
+        const popupImg = popup.querySelector(".pp-img");
+        //activate lorder until the popupImg loaded
+        popup.querySelector(".pp-loader").classList.add("active");
+        popupImg.src = imgSrc;
+        popupImg.onload = () =>{
+            //deactivate loard after the popImg loaded
+            popup.querySelector(".pp-loader").classList.remove("active");
+        }
+        popup.querySelector(".pp-counter").innerHTML = (slideIndex+1) + "of" +
+        screenshots.length;
+
+    }
+    //next slide
+    nextBtn.addEventListener("click", ()=>{
+        if(slideIndex === screenshots.length-1){
+            slideIndex = 0;
+        }else{
+            slideIndex++;
+        }
+        popupSlideshow();
+    })
+
+    prevBtn.addEventListener("click", ()=>{
+        if(slideIndex === 0){
+            slideIndex = screenshots.length-1
+        }else{
+            slideIndex--;
+        }
+        popupSlideshow();
+        
+    })
+
+    function popupDetails(){
+        //if portfolio-item-details not exists
+        if(!portfolioItems[itemIndex].querySelector(".portfolio-item-details")){
+            projectDetailsBtn.style.display="none";
+            return; //end function execution
+        }
+        projectDetailsBtn.style.display="block";
+
+        //get the project details
+        const details = portfolioItems[itemIndex].querySelector(".portfolio-item-details").innerHTML;
+        //set the project details
+        popup.querySelector(".pp-project-details").innerHTML=details;
+        //get the project title
+        const title = portfolioItems[itemIndex].querySelector(".portfolio-item-title").innerHTML;
+        //set the project title
+        popup.querySelector(".pp-title h2").innerHTML = title;
+        //get the project category
+        const category = portfolioItems[itemIndex].getAttribute("data-category");
+        //set the project category
+        popup.querySelector(".pp-project-category").innerHTML = category.split("-").join(" "); 
+   
+    }
+
+    projectDetailsBtn.addEventListener("click", ()=>{
+        popupDetailsToggle();
+    })
+
+    function popupDetailsToggle(){
+        if(projectDetailsContainer.classList.contains("active")){
+
+            projectDetailsBtn.querySelector("i").classList.remove("fa-minus");
+            projectDetailsBtn.querySelector("i").classList.add("fa-plus");
+
+            projectDetailsContainer.classList.remove("active");
+            projectDetailsContainer.style.maxHeight = 0 + "px"
+        }else{
+            projectDetailsBtn.querySelector("i").classList.remove("fa-plus");
+            projectDetailsBtn.querySelector("i").classList.add("fa-minus");
+
+
+          projectDetailsContainer.classList.add("active");
+          projectDetailsContainer.style.maxHeight = projectDetailsContainer.scrollHeight + "px";
+          popup.scrollTo(0,projectDetailsContainer.offsetTop);
+        }
     }
 
     
