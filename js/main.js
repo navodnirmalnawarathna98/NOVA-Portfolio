86c2528a528f0121fb1f3be5fd779141a2c28f46
Navod Nirmal Nawarathna <novanawarathna@gmail.com>
2022-07-17 16:52:42 +0530

    update
    update all

--- js/main.js
+++ js/main.js
@@ -1,3 +1,85 @@
+/*------------------navigation menu---------------*/
+(()=>{
+    const hamburgerBtn = document.querySelector(".hamburger-btn"),
+    navMenu = document.querySelector(".nav-menu"),
+    closeNavBtn = navMenu.querySelector(".close-nav-menu");
+
+    hamburgerBtn.addEventListener("click", showNavMenu);
+    closeNavBtn.addEventListener("click", hideNavMenu);
+
+    function showNavMenu(){
+        navMenu.classList.toggle("open");
+        bodyScrollingToggle();
+    }
+
+    function hideNavMenu(){
+        navMenu.classList.toggle("open");
+        fadeOutEffect();
+        bodyScrollingToggle();
+    }
+
+    function fadeOutEffect(){
+        document.querySelector(".fade-out-effect").classList.add("active");
+        setTimeout(()=>{
+            document.querySelector(".fade-out-effect").classList.remove("active");
+        },300)
+    }
+
+    //attach an event handler to document
+    document.addEventListener("click", (event)=>{
+        if(event.target.classList.contains('link-item')){
+
+            if(event.target.hash !==""){
+                event.preventDefault();
+                const hash = event.target.hash;
+
+                document.querySelector(".section.active").classList.add("hide");
+                document.querySelector(".section.active").classList.remove("active");
+
+                document.querySelector(hash).classList.add("active");
+                document.querySelector(hash).classList.remove("hide");
+
+                navMenu.querySelector(".active").classList.add("outer-shadow","hover-in-shadow");
+                navMenu.querySelector(".active").classList.remove("active","inner-shadow");
+                if(navMenu.classList.contains("open")){
+
+                    event.target.classList.add("active","inner-shadow");
+                    event.target.classList.remove("outer-shadow","hover-in-shadow");
+    
+                    hideNavMenu();
+                }else{
+                    let navItems = navMenu.querySelectorAll(".link-item");
+                    navItems.forEach((item)=>{
+                        if(hash === item.hash){
+                           item.classList.add("active","inner-shadow");
+                           item.classList.remove("outer-shadow", "hover-in-shadow");
+                        }
+                    })
+                    fadeOutEffect();
+                }
+
+                window.location.hash = hash;
+            }
+        }
+    })
+
+
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*----------about section tabs-------------*/
 (() =>{
     const aboutSection = document.querySelector(".about-section"),
@@ -230,3 +312,22 @@ function slider(){
 slider();
 
 })();
+
+
+ /*-----------hide all section excep active-----------------*/
+ (()=>{
+    const section = document.querySelectorAll(".section");
+    section.forEach((section)=>{
+        if(!section.classList.contains("active")){
+            section.classList.add("hide");
+        }
+    })
+ })();
+
+
+ window.addEventListener("load", () =>{
+    document.querySelector(".preloader").classList.add("fade-out");
+    setTimeout(() =>{
+        document.querySelector(".preloader").style.display="none";
+    },600)
+ })
